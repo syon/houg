b2824514a4b16f563d0a48480eee43418225c4ae
syon <syon.xiii@gmail.com>
2019-06-05 22:15:44 +0900

    :zap: export

---
+++ batch/export.js
@@ -0,0 +1,26 @@
+const stringifySync = require('csv-stringify/lib/sync')
+const Dao = require('../database/dao.node')
+
+async function main() {
+  const records = await Dao.selectAllSites()
+
+  const csvString = stringifySync(records, {
+    header: true,
+    columns: {
+      url: 'url',
+      name: 'name',
+      taxo: 'taxo',
+      desc: 'desc'
+    },
+    delimiter: '\t',
+    quoted_string: false
+  })
+
+  require('fs').writeFileSync('./batch/export.tsv', csvString)
+}
+
+;(async () => {
+  await main()
+})().catch(e => {
+  // Deal with the fact the chain failed
+})
--- database/dao.node.js
+++ database/dao.node.js
@@ -1,6 +1,18 @@
 const { db } = require('../database/admin.node')
 
 module.exports = class Dao {
+  static async selectAllSites() {
+    const path = `site`
+    const querySnapshot = await db.collection(path).get()
+    const dataSet = querySnapshot.docs.map(doc => {
+      // Metadata
+      const obj = doc.data()
+      obj.id = doc.id
+      return obj
+    })
+    return dataSet
+  }
+
   static async addSite(form) {
     console.log({ form })
     await db
